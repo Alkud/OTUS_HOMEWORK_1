ee4c7d34ba7cd5409cf4003b6d57504403dfb0ea
Alkud <alexandr_kudinov@mail.ru>
2018-02-28 13:12:01 +0300

    Check getVersion() declration

--- helper_functions.cpp
+++ helper_functions.cpp
@@ -2,7 +2,7 @@
 
 #include "helper_functions.h"
 
-void getVersion(int& versionMajor, int& versionMinor, int versionPatch)
+void getVersion(int& versionMajor, int& versionMinor, int& versionPatch)
 {
   versionMajor = OTUS_HW_1_VERSION_MAJOR;
   versionMinor = OTUS_HW_1_VERSION_MINOR;
